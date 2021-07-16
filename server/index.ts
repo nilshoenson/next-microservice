8f7b8bb6cba4bb9f1c57858a02245c763923213e
Nils Hoenson <nils@yummygum.com>
2021-07-17 01:36:57 +0200

    Set basic status

--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,18 @@ import next from 'next'
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+// const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
 
-  server.all('*', (req: Request, res: Response) => handle(req, res))
+  // server.all('*', (req: Request, res: Response) => {
+  //   handle(req, res)
+  // })
+
+  server.get('/api', (req: Request, res: Response) => {
+    res.json({ status: 'okay' })
+  })
 
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`)
