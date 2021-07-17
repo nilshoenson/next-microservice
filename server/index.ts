708af2ce3b3859c9099e2f8975e109bacb854abc
Nils Hoenson <nils@yummygum.com>
2021-07-17 02:10:29 +0200

    Another test

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,29 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import next from 'next'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+// const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
 
-  server.all('/', (req: Request, res: Response) => {
-    handle(req, res)
-  })
+  // server.all('/', (req: Request, res: Response) => {
+  //   handle(req, res)
+  // })
 
-  server.get('/api', (req: Request, res: Response) => {
-    res.json({ status: 'okay' })
-  })
+  // server.get('/api', (req: Request, res: Response) => {
+  //   res.json({ status: 'okay' })
+  // })
+
+  server.get('/', (req, res) => res.send('Home Page Route'))
+
+  server.get('/about', (req, res) => res.send('About Page Route'))
+
+  server.get('/portfolio', (req, res) => res.send('Portfolio Page Route'))
+
+  server.get('/contact', (req, res) => res.send('Contact Page Route'))
 
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`)
