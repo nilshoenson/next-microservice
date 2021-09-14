3f7e34115865bf73bffbbe794b8d39b9d2963197
nilshoenson <nils@yummygum.com>
2021-09-15 00:34:39 +0200

    Add main

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.use(express.json({ extended: false }))
 
 app.use("/api/product", product)
 
+app.get('/', (req, res) => {
+  res.json({ status: 'okay' });
+})
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
